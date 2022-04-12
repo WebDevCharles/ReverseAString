2c23366a77a22057e40b460eba364a787a422e82
Charles Hall <webdevcharles@gmail.com>
2022-04-12 14:42:30 -0400

    ReverseAString Update

--- js/site.js
+++ js/site.js
@@ -2,26 +2,47 @@
 function getMessage() {
 
     let msg = document.getElementById("txtMessage").value;
-    displayMessage(msg);
+
+    // Validate if the "msg" is not equal to an empty string.
+    if (msg == "") {
+        alert("You must enter a string!");
+
+    } else {
+        // Changing output to "lowercase" regardless of input.
+        msg = msg.toLowerCase();
+        let revMessage = reverseMessageB(msg);
+        displayMessage(revMessage);
+    }
+
+}
+
+function reverseMessageA(msg) {
+    let revMessage = "";
+
+    // Decrimenting loop
+    for (let index = msg.length - 1; index >= 0; index--) {
+        let char = msg[index];
+        revMessage += char;
+
+    }
+
+    return revMessage;
+}
+
+function reverseMessageB(msg) {
+
+    // Add multiple "split" and "join" methods
+    let revMessage = "";
+    revMessage = msg.split("").reverse().join("");
+    
+    return revMessage;
 
 }
 
 // Final Step - View
 function displayMessage(message) {
 
-    // First get the "ol" element from the page
     element = document.getElementById("results");
-
-    // Create a new "li" element
-    let item = document.createElement("li");
-
-    // Add classes to the "li" element
-    item.classList.add("list-group-item");
-
-    // Set classes to the "li" element
-    item.innerHTML = message;
-
-    // Add the new items to the list
-    element.appendChild(item) = item;
+    element.innerHTML = message;
 
 }
